5fb51a38ff659afaaff42565e1d2a378eddf10f8
Ignacio <ignacio.revainera@hotmail.com>
2024-03-07 22:37:57 -0300

    Add custom screen size and themes colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,49 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				xs: "436px",
+			},
+		},
 	},
 	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				dark: {
+					...require("daisyui/src/theming/themes")["dark"],
+					primary: "#FF006D",
+					secondary: "#00C6C2",
+					accent: "#005DFF",
+					neutral: "#18181b",
+					"neutral-content": "#ffffff",
+					"base-100": "#0a0a0a",
+					"base-200": "#0f0f0f",
+					"base-300": "#171717",
+					info: "#009FFF",
+					success: "#00AB44",
+					warning: "#eab308",
+					error: "#b91c1c",
+				},
+			},
+			{
+				light: {
+					...require("daisyui/src/theming/themes")["light"],
+					primary: "#FF006D",
+					secondary: "#9333ea",
+					accent: "#FFA200",
+					neutral: "#dadace",
+					"neutral-content": "#000000",
+					"base-100": "#F6F6F4",
+					"base-200": "#ecece8",
+					"base-300": "#d8d8d0",
+					info: "#009FFF",
+					success: "#00AB44",
+					warning: "#eab308",
+					error: "#b91c1c",
+				},
+			},
+		],
+	},
 };
